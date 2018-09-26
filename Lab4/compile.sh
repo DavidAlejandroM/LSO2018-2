1790dd932ce65285dcd16d5eb233943393b29500
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-09-26 10:04:35 -0500

    elminando los archivos compilados anteriormente

--- Lab4/compile.sh
+++ Lab4/compile.sh
@@ -1,3 +1,9 @@
+# eliminamos los archivos anteriores
+rm main.o psinfo.o psinfo.out
+
+# compilacion de los archivos .o
 gcc main.c -o main.o -c
 gcc psinfo.c -o psinfo.o -c
+
+# compilacion del proyecto
 gcc -o psinfo.out main.o psinfo.o
